feea11e2210805ee63f98861b97fb28d05b06982
sakistellina <69436794+sakistellina@users.noreply.github.com>
2020-12-13 12:31:49 +0100

    Added the GibHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,6 +3,23 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+  <div className="App">
+    <h1>Weather App</h1>
+      <footer>
+        Project by{" "}
+        <a href="https://www.youtube.com/channel/UCxA0jIEI19lY0Kg_jvcGSRQ"
+          target="_blank">
+          Sakistellina
+        </a>{" "} 
+          and is{" "}
+        <a 
+          href="https://github.com/sakistellina/react-weather-app"
+          target="_blank">
+          open-sourced on GibHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
