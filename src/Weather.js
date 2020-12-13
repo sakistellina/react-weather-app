435db7a2b1840c846785b0ad71805dac71c9294a
sakistellina <69436794+sakistellina@users.noreply.github.com>
2020-12-13 23:49:50 +0100

    Saved

--- src/Weather.js
+++ src/Weather.js
@@ -31,7 +31,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    const apiKey = "8f2a727a8d5b5cadf3134d55a1087a44";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
