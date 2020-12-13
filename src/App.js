93d7300b9ea5fc11914cbf2dcde09ec00ec7e350
sakistellina <69436794+sakistellina@users.noreply.github.com>
2020-12-13 22:54:06 +0100

    Added Weather skills

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import "./App.css";
 
 export default function App() {
   return (
-  <div className="App">
-    <div className="container">
-      <Weather />
-      <footer>
-        Project by{" "}
-        <a href="https://www.youtube.com/channel/UCxA0jIEI19lY0Kg_jvcGSRQ"
-          target="_blank">
-          Sakistellina
-        </a>{" "} 
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.delac.io/" target="_blank">
+            Matt Delac
+          </a>{" "}
           and is{" "}
-        <a 
-          href="https://github.com/sakistellina/react-weather-app"
-          target="_blank">
-          open-sourced on GibHub
-        </a>
-      </footer>
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
